fac9f76f38116857d10fb4c16ac4af6c66938548
epitalbot <93124225+epitalbot@users.noreply.github.com>
2022-05-02 21:51:41 +0200

    Adding marketCap

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ function updateInterval() {
                             })
                             .then(channel => {
                                 let marketCap = JPEGValue * supply;
+                                marketCap = marketCapFormatter(marketCap, 1);
                                 return channel.setName(`｜MC: ${marketCap}$`);
                             })
                 })
@@ -115,4 +116,21 @@ function addGuildToActive(guildId) {
             storage.set("activeGuilds", guilds).then(updateInterval)
         })
         .catch(console.error)
+}
+
+function marketCapFormatter(num, digits) {
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var item = lookup.slice().reverse().find(function(item) {
+        return num >= item.value;
+    });
+    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
 }
